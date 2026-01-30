7e85d704026e07583ca3ceb0bd778eb38590a346
KaiCarmin <kai.carmin1441@gmail.com>
2026-01-30 18:33:17 +0200

    update binder for handling vector5 changes

--- src/core/bindings.cpp
+++ src/core/bindings.cpp
@@ -11,25 +11,44 @@ PYBIND11_MODULE(_core, m) {
 
     // 1. Bind the State struct
     py::class_<State>(m, "State")
+        // 2D Constructor (existing)
         .def(py::init<double, double, double, double>(), 
              py::arg("rho"), py::arg("u"), py::arg("v"), py::arg("p"))
+        
+        // --- NEW: 3D Constructor ---
+        .def(py::init<double, double, double, double, double>(), 
+             py::arg("rho"), py::arg("u"), py::arg("v"), py::arg("w"), py::arg("p"))
+        
+        // Properties
         .def_readwrite("rho", &State::rho)
         .def_readwrite("u", &State::u)
         .def_readwrite("v", &State::v)
+        .def_readwrite("w", &State::w) // <-- New: expose Z-velocity
         .def_readwrite("p", &State::p)
+        
         .def("__repr__", [](const State &s) {
-            return "<State rho=" + std::to_string(s.rho) + " p=" + std::to_string(s.p) + ">";
+            return "<State rho=" + std::to_string(s.rho) + 
+                   " u=" + std::to_string(s.u) + 
+                   " v=" + std::to_string(s.v) + 
+                   " w=" + std::to_string(s.w) + 
+                   " p=" + std::to_string(s.p) + ">";
         });
 
-    // 2. Bind the Flux (Vector4) struct
+    // 2. Bind the Flux (Vector5) struct
     py::class_<Flux>(m, "Flux")
-        // We only need read access in Python for tests
-        .def_property_readonly("rho", [](const Flux& f){ return f[0]; })
-        .def_property_readonly("mom_x", [](const Flux& f){ return f[1]; })
-        .def_property_readonly("mom_y", [](const Flux& f){ return f[2]; })
-        .def_property_readonly("E", [](const Flux& f){ return f[3]; })
+        // We use lambdas to access the union members safely
+        .def_property_readonly("rho",   [](const Flux& f){ return f.rho; })
+        .def_property_readonly("mom_x", [](const Flux& f){ return f.mom_x; })
+        .def_property_readonly("mom_y", [](const Flux& f){ return f.mom_y; })
+        .def_property_readonly("mom_z", [](const Flux& f){ return f.mom_z; }) // <-- New
+        .def_property_readonly("E",     [](const Flux& f){ return f.E; })
+        
         .def("__repr__", [](const Flux &f) {
-            return "<Flux mass=" + std::to_string(f[0]) + " E=" + std::to_string(f[3]) + ">";
+            return "<Flux mass=" + std::to_string(f.rho) + 
+                   " mx=" + std::to_string(f.mom_x) + 
+                   " my=" + std::to_string(f.mom_y) + 
+                   " mz=" + std::to_string(f.mom_z) + 
+                   " E=" + std::to_string(f.E) + ">";
         });
 
     // 3. Bind the Solvers
